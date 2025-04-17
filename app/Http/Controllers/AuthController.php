9d1a3e9ca9dcd616e42859078e973322fc443c97
SnorlaxSPI <aflinsjr@icloud.com>
2025-04-17 20:45:31 -0300

    020

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -82,6 +82,8 @@ class AuthController extends Controller
 
     public function logout()
     {
-        echo 'Logout';
+        // logout from the application
+        session()->forget('user');
+        return redirect()->to('/login');
     }
 }
