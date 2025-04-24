0208ab46dd16a1eb14dcff54d93dc7275b41e85f
SnorlaxSPI <aflinsjr@icloud.com>
2025-04-23 21:34:54 -0300

    031

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
@@ -26,23 +27,13 @@ class MainController extends Controller
 
     public function editNote($id)
     {
-        $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
         echo "Edit note with ID: $id";
     }
 
     public function deleteNote($id)
     {
-        $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
         echo "Delete note with ID: $id";
-    }
-
-    public function decryptId($id)
-    {
-        try {
-            $id = Crypt::decrypt($id);
-        } catch (DecryptException $e) {
-            return redirect()->route('home');
-        }
-        echo "Decrypted note ID: $id";
     }
 }
