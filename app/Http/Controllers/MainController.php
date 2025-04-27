07b99ee8990957b1a6958017d7d229b80776b797
SnorlaxSPI <aflinsjr@icloud.com>
2025-04-26 21:19:48 -0300

    035

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -71,6 +71,51 @@ class MainController extends Controller
         return view('edit_note', ['note' => $note]);
     }
 
+    public function editNoteSubmit(Request $request)
+    {
+        // validate request
+        $request->validate(
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000',
+            ],
+            // error messages
+            [
+                'text_title.required' => 'O título é obrigatório',
+                'text_title.min' => 'O título deve ter no mínimo :min caracteres',
+                'text_title.max' => 'O título deve ter no máximo :max caracteres',
+                'text_note.required' => 'A nota é obrigatória',
+                'text_note.min' => 'A nota deve ter no mínimo :min caracteres',
+                'text_note.max' => 'A nota deve ter no máximo :max caracteres',
+            ]
+        );
+
+        // check if note_id is set
+        if ($request->note_id == null) {
+            return redirect()->route('home');
+        }
+
+        //decrypt note_id 
+        $id = Operations::decryptId($request->note_id);
+
+        // load note
+        $note = Note::find($id);
+
+        // get note id
+        $id = $request->input('note_id');
+        $id = Operations::decryptId($id);
+
+        // update note
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        // redirect to home
+        return redirect()
+            ->route('home')
+            ->with('success', 'Nota editada com sucesso');
+    }
+
     public function deleteNote($id)
     {
         $id = Operations::decryptId($id);
